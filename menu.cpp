408e67bdc757d79fbe6f7486812144d57e533420
Mohamed Alsayed Lashin <sayedlashine1@gmail.com>
2025-09-24 00:29:34 +0300

    making all the filters in menu file as functions + crop file is not almost finished

--- menu.cpp
+++ menu.cpp
@@ -1,11 +1,187 @@
 #include <iostream>
 #include <string>
 #include "Image_Class.h"
-#include "Grayscale filter.cpp"
-#include "Black and White filter.cpp"
-#include "Invert Image filter.cpp"
-#include "flip filter.cpp"
+#include "Crop image.cpp"
 using namespace std;
+//<<------------------------1------------------------->>
+// the first step in the project
+// converting a colored image to gray scale image
+void GrayscaleConversion(string filename)
+{
+    Image image(filename);
+
+    for (int i = 0; i < image.width; ++i)
+    {
+        for (int j = 0; j < image.height; ++j)
+        {
+            unsigned int avg = 0;
+
+            for (int k = 0; k < 3; ++k)
+            {
+                avg += image(i, j, k);
+            }
+
+            avg /= 3;
+
+            image(i, j, 0) = avg;
+            image(i, j, 1) = avg;
+            image(i, j, 2) = avg;
+        }
+    }
+
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    cin >> filename;
+    image.saveImage(filename);
+    cout << "Image saved successfully as " << filename << endl;
+}
+//<<------------------------2------------------------->>
+// the second filter in our project
+//  converting a colored image to black and white image
+int BlackWhiteConvertion(string filename)
+{
+    Image image(filename);
+
+    for (int i = 0; i < image.width; ++i)
+    {
+        for (int j = 0; j < image.height; ++j)
+        {
+            unsigned int average = 0;
+
+            for (int k = 0; k < 3; ++k)
+            {
+                average += image(i, j, k);
+            }
+
+            average /= 3;
+            if (average >= 128)
+            {
+                image(i, j, 0) = 255;
+                image(i, j, 1) = 255;
+                image(i, j, 2) = 255;
+            }
+            else
+            {
+                image(i, j, 0) = 0;
+                image(i, j, 1) = 0;
+                image(i, j, 2) = 0;
+            }
+        }
+    }
+
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    cin >> filename;
+    image.saveImage(filename);
+    cout << "Image saved successfully as " << filename << endl;
+    return 0;
+}
+//<<------------------------3------------------------->>
+// the third filter in our project
+//  Invert the Image colors
+int invertimage(string filename)
+{
+    Image image(filename);
+
+    for (int i = 0; i < image.width; ++i)
+    {
+        for (int j = 0; j < image.height; ++j)
+        {
+
+            for (int k = 0; k < 3; ++k)
+            {
+                image(i, j, k) = 255 - image(i, j, k);
+            }
+        }
+    }
+
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    cin >> filename;
+    image.saveImage(filename);
+    cout << "Image saved successfully as " << filename << endl;
+    return 0;
+}
+//<<------------------------4------------------------->>
+// the fourth filter in our project
+//  flip the Image vertically and horizontally
+void Verticalflip(string imageVflip)
+{
+    Image image(imageVflip);
+
+    for (int i = 0; i < image.height / 2; ++i)
+    {
+        for (int j = 0; j < image.width; ++j)
+        {
+            pixel tempR = image.getPixel(j, i, 0);
+            pixel tempB = image.getPixel(j, i, 1);
+            pixel tempG = image.getPixel(j, i, 2);
+            image(j, i, 0) = image.getPixel(j, image.height - i - 1, 0);
+            image(j, i, 1) = image.getPixel(j, image.height - i - 1, 1);
+            image(j, i, 2) = image.getPixel(j, image.height - i - 1, 2);
+            image(j, image.height - i - 1, 0) = tempR;
+            image(j, image.height - i - 1, 1) = tempB;
+            image(j, image.height - i - 1, 2) = tempG;
+        }
+    }
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    cin >> imageVflip;
+    image.saveImage(imageVflip);
+    cout << "Image saved successfully as " << imageVflip << endl;
+}
+void Horizontalflip(string imageHflip)
+{
+    Image image(imageHflip);
+
+    for (int i = 0; i < image.width / 2; ++i)
+    {
+        for (int j = 0; j < image.height; ++j)
+        {
+            pixel tempR = image.getPixel(i, j, 0);
+            pixel tempB = image.getPixel(i, j, 1);
+            pixel tempG = image.getPixel(i, j, 2);
+            image(i, j, 0) = image.getPixel(image.width - 1 - i, j, 0);
+            image(i, j, 1) = image.getPixel(image.width - 1 - i, j, 1);
+            image(i, j, 2) = image.getPixel(image.width - 1 - i, j, 2);
+            image(image.width - 1 - i, j, 0) = tempR;
+            image(image.width - 1 - i, j, 1) = tempB;
+            image(image.width - 1 - i, j, 2) = tempG;
+        }
+    }
+    cout << "Pls enter image name to store new image\n";
+    cout << "and specify extension .jpg, .bmp, .png, .tga: ";
+
+    cin >> imageHflip;
+    image.saveImage(imageHflip);
+    cout << "Image saved successfully as " << imageHflip << endl;
+}
+int flipimage(string filename)
+{
+    cout << "Which flip do you want?\n";
+    cout << "1- Vertical flip\n";
+    cout << "2- Horizontal flip\n";
+    cout << "Your choice: ";
+    string choose;
+    cin >> choose;
+    if (choose == "1" || choose == "Vertical flip")
+    {
+        Verticalflip(filename);
+    }
+    else if (choose == "2" || choose == "Horizontal flip")
+    {
+        Horizontalflip(filename);
+    }
+    else
+    {
+        cout << "Invalid choice. Please choose 1 or 2." << endl;
+    }
+    return 0;
+}
 
 //-------------------menu----------------------
 
@@ -29,6 +205,7 @@ int main()
     cout << "2- Black and White\n";
     cout << "3- Invert Image colors\n";
     cout << "4- Flip Image\n";
+    cout << "5- Crop Image\n";
     cout << "Your choice: ";
     getline(cin, choose);
     if (choose == "1" || choose == "Grayscale Conversion")
@@ -46,5 +223,9 @@ int main()
     else if (choose == "4" || choose == "Flip Image")
     {
         flipimage(imagename);
+    }
+    else if (choose == "5" || choose == "Crop Image")
+    {
+        CropImage(imagename);
     }
 }
